411a69ea94305215118fbf0f1eb1b395ce73af58
MineOwl <minegishirei@gmail.com>
2021-04-23 07:12:33 +0900

    test

--- django/cmd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$ENV" = 'DEV' ]; then
-  echo "Running Development Server"
-else
-  echo "Running Production Server"
-  #exec python3 manage.py runserver 0.0.0.0:5000 
-fi
