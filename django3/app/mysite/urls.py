ac40f785850942a6558ee10fd6691579cdb1dd77
MineOwl <minegishirei@gmail.com>
2021-04-24 00:09:43 +0900

    add dark mode

--- django3/app/mysite/urls.py
+++ django3/app/mysite/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("home.urls")),
     path("", include("engineer.urls"))
 ]
