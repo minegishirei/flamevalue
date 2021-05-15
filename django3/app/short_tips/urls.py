d92bd065e110d4ce96a744cba4c124125a6f71ef
MineOwl <minegishirei@gmail.com>
2021-05-15 11:39:26 +0900

    normal

--- django3/app/short_tips/urls.py
+++ django3/app/short_tips/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 switchPage = views.SwitchPage()
 urlpatterns = [
-    path('', switchPage.main , name="index"),
+    path('', switchPage.index , name="index"),
     path("index.html" , views.index , name=''),
     path('table_index.html', switchPage.table_index , name="table_index"),
     path('buildable_index.html', switchPage.index , name="buildable_index"),
