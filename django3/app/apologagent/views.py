493eac14e2e3e2ab7cc5cc1dc140a045cf7cfee6
MineOwl <minegishirei@gmail.com>
2021-06-19 19:23:05 +0900

    normal

--- django3/app/apologagent/views.py
+++ django3/app/apologagent/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 def index(request):
     params = {
         "title" : "反省書自動作成ツール🙇‍♂️",
+        "description" : "面倒な反省文をあなたの代わりに作ります。遅刻した時、寝坊した時、居眠りしてしまった時に、どうぞ。",
         "favicon" : "/static/チャット.png"
     }
     return render(request,"apologagent/index.html",params)
 
 def page(request, htmlname):
+    params = {
+        "title" : "反省書自動作成ツール🙇‍♂️",
+        "description" : "面倒な反省文をあなたの代わりに作ります。遅刻した時、寝坊した時、居眠りしてしまった時に、どうぞ。",
+        "favicon" : "/static/チャット.png"
+    }
     return render(request, f"apologagent/page/{htmlname}")
 
