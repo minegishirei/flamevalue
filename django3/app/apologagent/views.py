b51ec4b99d9b288e0c30c888fea25f39c8b693a4
MineOwl <minegishirei@gmail.com>
2021-06-19 19:07:08 +0900

    normal

--- django3/app/apologagent/views.py
+++ django3/app/apologagent/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     params = {
-        "title" : "反省書自動作成ツール"
+        "title" : "反省書自動作成ツール🙇‍♂️",
+        "favicon" : "/static/チャット.png"
     }
-    return render(request,"apologagent/index.html")
+    return render(request,"apologagent/index.html",params)
 
 def page(request, htmlname):
     return render(request, f"apologagent/page/{htmlname}")
