f76b2cb56c4687188d33d4297cb4001d127ff8a5
MineOwl <minegishirei@gmail.com>
2021-06-20 20:37:16 +0900

    normal

--- django3/app/apologagent/views.py
+++ django3/app/apologagent/views.py
@@ -7,7 +7,8 @@ def index(request):
         "title" : "反省書自動作成ツール🙇‍♂️",
         "description" : "面倒な反省文をあなたの代わりに作ります。遅刻した時、寝坊した時、居眠りしてしまった時に、どうぞ。",
         "favicon" : "/static/チャット.png",
-        "img": "thumbnail.png"
+        "img": "/static/thumbnail.png"
+
     }
     return render(request,"apologagent/index.html",params)
 
@@ -37,7 +38,7 @@ def page(request, htmlname):
         "title" : "反省書エディター🙇‍♂️",
         "description" : "面倒な反省文をあなたの代わりに作ります。遅刻した時、寝坊した時、居眠りしてしまった時に、どうぞ。",
         "favicon" : "/static/チャット.png",
-        "img": "thumbnail.png"
+        "img": "/static/thumbnail.png"
     }
     value = getSessionValue(request, "deleteAll")
     if len(value) > 1 and ("transition" in session):
