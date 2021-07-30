bf2e5139faaec8a7b459232ad5062929b553340c
kawadasatoshi <minegishirei@gmail.com>
2021-07-30 22:39:03 +0900

    test

--- django3/app/oversea_it/views.py
+++ django3/app/oversea_it/views.py
@@ -107,9 +107,6 @@ def index(request):
     return render(request,"oversea_it/base/index.html",params)
 
 
-def sitemap():
-    pass
-
 
 def robots(request):
     return render(request, f"robots.txt")
@@ -160,8 +157,13 @@ def checkandrenew():
 
 
 def sitemap(request):
+    dt_now = datetime.datetime.now()
+    now = dt_now.strftime('%Y%m%d%H')
+    pop_page_list_copy = pop_page_list.copy()
+    for page in pop_page_list_copy:
+        page["lastmod"] = f"{dt_now.strftime('%Y')}-{dt_now.strftime('%m')}-{dt_now.strftime('%d')}T00:00:00+00:00" #"2021-07-30T13:25:37+00:00"
     params = {
-        "pop_page_list" : pop_page_list
+        "pop_page_list" : pop_page_list_copy
     }
     return render(request,f"oversea_it/sitemap.xml", params)
 
