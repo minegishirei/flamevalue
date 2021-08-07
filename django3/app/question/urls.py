1d4e6ebb7ac1903149c1c549c7d11c8ed9f67e88
kawadasatoshi <minegishirei@gmail.com>
2021-08-07 13:17:25 +0900

    test

--- django3/app/question/urls.py
+++ django3/app/question/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path("", views.index ,name="index"),
     path("index.html", views.index ,name="pop_page"),
-    path("<page>", views.sitemap, name="page"),
+    path("sitemap.xml", views.sitemap, name="sitemap"),
     path("<category>/", views.category_list, name="category"),
     path("<category>/<yahoo_id>", views.page, name="page"),
 
