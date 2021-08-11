4afcf5b6c9f54b6483a842f6534c744bceb0409a
kawadasatoshi <minegishirei@gmail.com>
2021-08-11 12:21:38 +0900

    test

--- django3/app/short_tips/urls.py
+++ django3/app/short_tips/urls.py
@@ -4,6 +4,7 @@ switchPage = views.SwitchPage()
 urlpatterns = [
     path('', switchPage.index , name="index"),
     path("index.html" , switchPage.index , name=''),
+    path("robots.txt", switchPage.robots, name="robots"),
     path('table_index.html', switchPage.table_index , name="table_index"),
     path("sql.html", switchPage.sql , name=""),
     path("sitemap.xml", switchPage.sitemap , name= ""),
