74dd5f1a3f612965d6373bdce6c3829b519b694b
kawadasatoshi <minegishirei@gmail.com>
2021-08-15 09:03:11 +0900

    test

--- django3/app/oreilly/views.py
+++ django3/app/oreilly/views.py
@@ -117,8 +117,8 @@ def page(request, htmlpage):
             'good' : 0
         }
         params = paramFactory.build( htmlpage, context)
-        del request.POST["title"]
-        del request.POST["comment"]
+        request.POST["title"] = False
+        request.POST["comment"] = False
     else:
         params = paramFactory.build( htmlpage)
     
