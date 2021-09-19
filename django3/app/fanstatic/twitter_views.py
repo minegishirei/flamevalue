703791ec549cf101d6ae155c843d04f9716a331b
kawadasatoshi <minegishirei@gmail.com>
2021-09-19 14:21:40 +0900

    test

---
+++ django3/app/fanstatic/twitter_views.py
@@ -0,0 +1,119 @@
+from django.shortcuts import render, redirect
+import json
+import sys
+if '/God' not in sys.path:
+    sys.path.append('/God')
+import Twitter
+import Github
+import datetime
+import NatureLang
+import Sitemap
+
+
+
+
+
+repo = "twitter_network"
+
+
+
+def index(request, htmlname):
+    return page()
+
+def page(request, htmlname):
+    if "reload" in request.GET:
+        Github.delete_page(repo, htmlname)
+    upload_network_json(htmlname)
+    params = {
+        "acount_name" : htmlname
+    }
+    return render(request, "fanstatic/dashboard/twitter_network.html", params)
+
+
+def upload_network_json(htmlname):
+    if not Github.has_already_created(repo, htmlname):
+        git_json = create_network_json(htmlname)
+        text = json.dumps(git_json, ensure_ascii=False, indent=4)
+        Github.upload(repo, htmlname, text)
+
+
+
+
+
+def create_network_json(root_name):
+    root_name = "@" + root_name.replace("@","")
+    link_list = []
+    acount_list = []
+    acount_set = set()
+    
+    myTwitterAction = Twitter.MyTwitterAction()
+    def induction_json(parent_name, depth, node_num):
+        if depth < 0:
+            return
+        tweet_list = myTwitterAction.search_tweet_list(parent_name, amount=100)
+        dub_tweet_list, non_dub_tweet_list = get_dub_acount(tweet_list, acount_set)
+
+        for tweet in dub_tweet_list:
+            acount_name = "@"+tweet["user"]["screen_name"]
+            if acount_name in acount_set:
+                pass
+            else:
+                acount_set.add(acount_name)
+                acount_list.append(grep_node_info(tweet))
+            link_list.append({
+                "target" : acount_name,
+                "source" : parent_name,
+                "value"  : 1
+            })
+            induction_json(acount_name, depth-1, int(node_num/2))
+        
+        for tweet in non_dub_tweet_list[:max(0, node_num -len(dub_tweet_list))]:
+            acount_name = "@"+tweet["user"]["screen_name"]
+            if acount_name in acount_set:
+                pass
+            else:
+                acount_set.add(acount_name)
+                acount_list.append(grep_node_info(tweet))
+            link_list.append({
+                "target" : acount_name,
+                "source" : parent_name,
+                "value"  : 1
+            })
+            induction_json(acount_name, depth-1, int(node_num/2))
+
+
+    def grep_node_info(tweet):
+        base = {
+            "name"  : "@" +tweet["user"]["screen_name"],
+            "img"   : tweet["user"]["profile_image_url"],
+            "text"  : tweet["text"],
+            "group" : 1
+        }
+        return base
+    
+    def get_dub_acount(tweet_list, acount_set):
+        dub_tweet_list = []
+        non_dub_tweet_list = []
+        for tweet in tweet_list:
+            acount_name = "@"+ tweet["user"]["screen_name"]
+            if acount_name in acount_set:
+                dub_tweet_list.append(tweet)
+            else:
+                non_dub_tweet_list.append(tweet)
+        return dub_tweet_list, non_dub_tweet_list
+    
+    induction_json(root_name, 1, 50)
+    if root_name not in acount_set:
+        acount_list.append({
+            "name"  : root_name,
+            "img"   : "https://cdn.icon-icons.com/icons2/1144/PNG/512/twitterlogo1_80940.png",
+            "text"  : "本人",
+            "group" : 1
+        })
+    return {
+        "nodes":acount_list,
+        "links":link_list
+    }
+
+
+#twitterのJsonをフォーマットしてくれる物が欲しい
--- django3/app/fanstatic/urls.py
+++ django3/app/fanstatic/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from . import twitter_views
 urlpatterns = [
     path('', views.index , name="index"), 
     path("index.html", views.index ,name="pop_page"),
     path("sitemap.xml", views.sitemap, name="sitemap"),
     path("pop_page.html", views.pop_page ,name="pop_page"),
     path("all_page.html", views.all_page ,name="all_page"),
+    path("twitter_network/<htmlname>", twitter_views.page ,name="twitter_network"),
     path("page/<htmlname>/data_loading.html", views.data_loading, name="data_loading"),
     path('page/<htmlname>/<pagetype>', views.page , name="index"),
     
