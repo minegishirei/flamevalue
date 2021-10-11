92d5df74c6aa65e22762a6d40cb4b93b3935151f
kawadasatoshi <minegishirei@gmail.com>
2021-10-11 14:38:02 +0900

    test

--- django3/nginx/start.sh
+++ django3/nginx/start.sh
@@ -25,7 +25,7 @@ for HOST in ${LETSENCRYPT_HOSTS}
 do
   if [ ! -e "/etc/letsencrypt/initialize" ]; then
     rm -rf /etc/letsencrypt/live/${HOST}
-    certbot certonly -n --keep-until-expiring --agree-tos \
+    certbot certonly --nginx -n --keep-until-expiring --agree-tos \
       --webroot --webroot-path /var/lib/letsencrypt \
       -m ${LETSENCRYPT_MAIL} -d ${HOST}
   fi
@@ -40,3 +40,6 @@ while true
 do
     sleep 7
 done
+
+
+#certbot certonly --agree-tos --webroot -w /var/lib/letsencrypt -d short-tips.info　-d www.short-tips.info
