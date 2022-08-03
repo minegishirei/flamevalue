f05bdfa2eca45d2ca4326797c3fd99e682a59702
kawadasatoshi <minegishirei@gmail.com>
2022-08-03 20:21:33 +0900

    test

--- django3/app/techblog/urls.py
+++ django3/app/techblog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from . import views
+from . import views, views_markdown
 urlpatterns = [
     path("", views.index ,name="index"),
     path("index.html", views.index ,name="index"),
     path("robots.txt", views.robots ,name="robots"),
     path("sitemap.xml", views.sitemap, name="sitemap"),
+    path("markdown_check.html", views_markdown.markdown_check, name="markdown"),
+    path("edit.html", views_markdown.edit, name="edit"),
     path("<category>/<htmlname>", views.page, name="page"),
     path("about.html/", views.about, name="about"),
-    path("<category_name>/", views.category_page, name="page")
+    path("<category_name>/", views.category_page, name="page"),
 ]
 
