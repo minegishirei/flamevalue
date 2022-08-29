81a2e7628cfa550c9697768c41f96b08e7641b84
kawadasatoshi <minegishirei@gmail.com>
2022-08-30 07:14:11 +0900

    test

--- django3/app/short_tips/urls.py
+++ django3/app/short_tips/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("index.html", views.index ,name="index"),
     path("robots.txt", views.robots ,name="robots"),
     path("sitemap.xml", views.sitemap, name="sitemap"),
+    path("sitemap", views.sitemap, name="s"),
     path("<category>/<htmlname>", views.page, name="page"),
     path("about.html/", views.about, name="about"),
     path("<category_name>/", views.category_page, name="page")
