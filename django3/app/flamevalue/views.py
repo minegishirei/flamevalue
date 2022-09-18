e93a9e566dad8f2751d8e00ddcb1a9b4be7b6984
kawadasatoshi <minegishirei@gmail.com>
2022-09-18 14:38:12 +0900

    test

--- django3/app/flamevalue/views.py
+++ django3/app/flamevalue/views.py
@@ -229,6 +229,17 @@ def page(request, htmlname):
     else:
         param = build_param(name)
         jsonIO.write(param["name"],param)
+    GET_active = request.GET.get("active")
+    if GET_active == "jobs":
+        param.update({
+            "title" : f"{name} の「採用情報」  FlameValue",
+            "description" : f"{name}株式会社の採用情報です。FlameValueは{name}の採用情報・求人情報を掲載しています。"
+        })
+    elif GET_active == "comments":
+        param.update({
+            "title" : f"{name}の「すべてのクチコミ」 FlameValue",
+            "description" : f"{name}ユーザーによる「すべての開発者クチコミ」のクチコミ・評価レビュー。{name}の採用を検討されている方が、{name}の「すべての開発者クチコミ」を把握するための参考情報として、{name}を使用した開発者から「すべての開発者クチコミ」に関するクチコミを収集し掲載しています。就職・採用活動での一段深めた開発者リサーチにご活用いただけます。"
+        })
     return render(request, f"jobstatic_pages/page.html", param)
 
 def index(request):
